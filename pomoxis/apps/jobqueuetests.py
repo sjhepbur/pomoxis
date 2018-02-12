044f96113fd2b7737ae745e37c42b1fb37da9589
Jayson Pidgeon - P Gordon student <jayson@magenta.chgi.ucalgary.ca>
2018-02-12 16:43:03 -0700

    Setup to perform unit tests on job queue

--- pomoxis/apps/jobqueuetests.py
+++ pomoxis/apps/jobqueuetests.py
@@ -1,28 +1,34 @@
-from timeout_decorator import *
-from lifojobqueue import *
+import unittest
+import time
+from timeout_decorator import timeout
+from lifojobqueue import TaskQueue
 
-def tests():
-    #Can change the timeout time. Make sure to use use_signals=False for multithreaded systems
-    @timeout(10, use_signals=False)
-    def blokkah(*args, **kwargs):
-        time.sleep(5)
-        print("Blookah " + str(args[0]))
+class TestLIFOJobQueue(unittest.TestCase):
+    def setUp(self):
+        self.actualList = []
 
+    @timeout(60)
+    def tests(self):
+        #Can change the timeout time. Make sure to use use_signals=False for multithreaded systems
+        expectedList = [0,1,5,6,9,8,7,4,3,2]
+        @timeout(10, use_signals=False)
+        def blokkah(*args, **kwargs):
+            time.sleep(5)
+            print("Blookah " + str(args[0]))
 
-    q = TaskQueue(num_workers=2, maxlength=10)
+        q = TaskQueue(num_workers=2, maxlength=10)
 
-    for item in range(10):
-        time.sleep(1)
-        if len(q) >= q.maxlen:
-            i = q.popleft()
-            print("Oh no, the queue is getting too big removing the oldest job "
-                + str(i[1][0]))
+        for item in range(10):
+            time.sleep(0.9)
+            if len(q) >= q.maxlen:
+                i = q.popleft()
+                self.actualList.append(i[1][0])
+                print("Oh no, the queue is getting too big removing the oldest job "
+                    + str(i[1][0]))
+            q.add_task(blokkah, item)
 
-        q.add_task(blokkah, item)
-        print("Added Item: " + str(item))
+        raw_input("Hit enter to continue")
 
-    raw_input("Press Enter to continue...\n")
-    print ("All done!")
 
 if __name__ == "__main__":
-    tests()
+    unittest.main()
